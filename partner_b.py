7961c959fe0fc854f32a1b539227fe2d90826524
Liam Abrams <liamtabrams@gmail.com>
2026-06-30 14:13:35 -0700

    Add in ValueError handling for counting up of scores input data

--- partner_b.py
+++ partner_b.py
@@ -33,9 +33,12 @@ scores = ['88', '95', 'absent', '72', 'n/a', '84']
 valid = []
 
 for i, s in enumerate(scores):
-    score = int(s)            # <-- this line crashes on bad strings
-    print(f'Score [{i}]: {score}')
-    valid.append(score)
+    try:
+        score = int(s)            # <-- this line crashes on bad strings
+        print(f'Score [{i}]: {score}')
+        valid.append(score)
+    except ValueError:
+        print(f'[index {i}]: Skipped bad score:', r"'{}'".format(s))
 
 print(f'Total of valid scores: {sum(valid)}')
 print(f'Count of valid scores: {len(valid)}')
